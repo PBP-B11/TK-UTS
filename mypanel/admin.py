7ef6b4ed62dc3eb5702614fe306f7b7cb9af15e5
erlanggak <dreakhadafi@gmail.com>
2022-10-27 20:11:08 +0700

    add address & is technician and add quantityinc/dec

--- mypanel/admin.py
+++ mypanel/admin.py
@@ -1,11 +1,8 @@
 from django.contrib import admin
 from mypanel.models import *
-from mycart.models import *
 
 admin.site.register(Customer)
 admin.site.register(Product)
-admin.site.register(Order)
-admin.site.register(OrderItem)
 admin.site.register(Inverter)
 admin.site.register(Panel)
 admin.site.register(Battery)
--- pbp_b11_project/settings.py
+++ pbp_b11_project/settings.py
@@ -137,9 +137,9 @@ STATICFILES_DIRS = [
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-MEDIA_URL = '/images/'
+MEDIA_URL = '/media/' 
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
