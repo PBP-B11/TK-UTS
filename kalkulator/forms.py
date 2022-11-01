76fa5bdb3335ee76c2e5912eb70ad68d32062041
Rafi Madani <rafimadani@Rafis-MacBook-Pro-2.local>
2022-11-01 16:21:34 +0700

    sbnrnya gaada bedanya si dengan commit sebelumnya, cuman yaudalaya

--- kalkulator/forms.py
+++ kalkulator/forms.py
@@ -1,7 +1,12 @@
 from django import forms
 from kalkulator.models import Calculation
-class AddHistory(forms.ModelForm):
-    class Meta:
-        model = Calculation
-        fields = {'electricity','offset','envfactor','roofarea'}  
-         #size estimate sama panel ga dimasukin ke forms, soalnya itu output
+class AddHistory(forms.Form):
+    model = Calculation
+    electricity = forms.IntegerField(label="")
+    offset = forms.IntegerField(label="")
+    envfactor = forms.IntegerField(label="")
+    roofarea = forms.IntegerField(label="")
+    
+    
+    
+    #size estimate sama panel ga dimasukin ke forms, soalnya itu output
