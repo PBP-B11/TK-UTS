5d9b8558065753a95ab88b560a411bbea63034e4
Ruzain <muhammadruzain10@gmail.com>
2022-12-07 04:48:42 +0700

    menambahakan data is_technician sebagai response dari login request

--- authentication/views.py
+++ authentication/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login as auth_login
+from django.http import HttpResponse
 from django.http import JsonResponse
+
+from django.core import serializers
 from django.views.decorators.csrf import csrf_exempt
+
+from mypanel.models import Customer
+
 
 @csrf_exempt
 def login(request):
@@ -12,11 +18,19 @@ def login(request):
         if user.is_active:
             auth_login(request, user)
             # Redirect to a success page.
+
+            customer = Customer.objects.get(user=request.user)
+
             return JsonResponse({
             "status": True,
-            "message": "Successfully Logged In!"
+            "message": "Successfully Logged In!",
             # Insert any extra data if you want to pass data to Flutter
+            "is_technician": customer.is_technician,
             }, status=200)
+
+
+
+            # return( Customer.objects.get(user=request.user).name)
         else:
             return JsonResponse({
             "status": False,
